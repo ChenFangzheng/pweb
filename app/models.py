f5e886d3b00d8dc6d269daba444079126593fc0d
Fangzheng <chenfangzheng@outlook.com>
2018-01-26 10:21:58 +0800

    add role model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,76 @@ from datetime import datetime
 from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import Serializer
-from flask_login import UserMixin, AnonymousUserMixin
+from flask_login import UserMixin
 from . import db, login_manager
+
+
+class Permission:
+    VIEW_JC = 1
+    VIEW_YU = 2
+    VIEW_MN = 4
+    EDIT_DATA = 8
+    ADMIN = 16
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.VIEW_JC, Permission.VIEW_YU, Permission.VIEW_MN],
+            'Moderator': [Permission.VIEW_MN, Permission.VIEW_YU, Permission.VIEW_JC, Permission.EDIT_DATA],
+            'Administrator': [Permission.VIEW_MN, Permission.VIEW_YU, Permission.VIEW_JC, Permission.EDIT_DATA,
+                              Permission.ADMIN]
+        }
+
+        default_role = 'User'
+
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permission()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     password_changed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
@@ -18,6 +80,11 @@ class User(UserMixin, db.Model):
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
@@ -34,6 +101,12 @@ class User(UserMixin, db.Model):
         s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
         return s.dumps({'id': self.id}).decode('utf-8')
 
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
     @staticmethod
     def verify_auth_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -45,21 +118,15 @@ class User(UserMixin, db.Model):
 
     @staticmethod
     def insert_administrator():
-        admin = User(username='admin', password='123456')
-        db.session.add(admin)
-        db.session.commit()
+        admin = User.query.filter_by(username='admin').first()
+        if admin is None:
+            admin = User(username='admin', password='123456')
+            admin.role = Role.query.filter_by(name='Administrator').first()
+            db.session.add(admin)
+            db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.username
-
-
-class AnonymousUser(AnonymousUserMixin):
-
-    def is_administrator(self):
-        return False
-
-
-login_manager.anonymous_user = AnonymousUser
 
 
 @login_manager.user_loader
--- pweb.py
+++ pweb.py
@@ -1,11 +1,12 @@
 import os
 from app import create_app, db
-from app.models import User
+from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 app.run(debug=True)
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User)
+    return dict(db=db, User=User, Role=Role)
